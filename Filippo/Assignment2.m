6e306268ee0a62e51656405da6023970953e90f0
Eleonora-Domenichelli <eleonorad0203@gmail.com>
2024-12-22 20:40:29 +0100

    Update Assignment2.m

---
+++ Filippo/Assignment2.m
@@ -0,0 +1,31 @@
+%% Main script for Planetary Explorer Mission assignment %%
+%
+% Group number : 2427
+%
+%--------------------------------------------------------------------------
+% Created and maintained by : 
+%
+% Azevedo Da Silva Esteban
+% Gavidia Pantoja Maria Paulina
+% Donati Filippo 
+% Domenichelli Eleonora
+% 
+%--------------------------------------------------------------------------
+% LAST UPDATE: 22-12-2024
+%
+
+% 2427 / Earth / J2 & Moon / STARTING DATE OF PROPAGATION? 
+
+%% SCRIPT INITIALISATION
+clc
+clear variables
+close all
+
+%% PRESENTATION
+fprintf('----------------------------------------------\n');
+fprintf('   PLANETARY EXPLORER MISSION ASSIGNMENT      \n');
+fprintf('----------------------------------------------\n');
+fprintf('\n');
+
+%% ORBIT CHARACTERISATION
+a = 23300; % Semi-major axis [km]
