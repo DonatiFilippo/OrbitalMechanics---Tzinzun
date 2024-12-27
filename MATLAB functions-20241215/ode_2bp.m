0cce68c0c18063a6ab0f6e696084c80824e4ec9a
Maria Paulina <75094629+MPaulinaPavidia@users.noreply.github.com>
2024-12-27 07:26:30 +0100

    Update ode_2bp.m

--- MATLAB functions-20241215/ode_2bp.m
+++ MATLAB functions-20241215/ode_2bp.m
@@ -1,4 +1,35 @@
 function dy = ode_2bp( ~ , y , mu )
+%% FUNCTION DESCRIPTION
+%
+%--------------------------------------------------------------------------
+% DESCRIPTION:
+% This function defines the system of ordinary differential equations (ODEs) 
+% for the two-body problem (Keplerian motion). It calculates the derivatives 
+% of the state vector (position and velocity) under the influence of a central 
+% gravitational force.
+%
+%--------------------------------------------------------------------------
+% INPUTS:
+%   ~         [1x1]   Time variable (not used since the system is autonomous) [-]
+%   y         [6x1]   State vector (position and velocity):
+%                     [rx, ry, rz, vx, vy, vz] [km, km/s]
+%   mu        [1x1]   Gravitational parameter of the central body     [km^3/s^2]
+%
+%--------------------------------------------------------------------------
+% OUTPUTS:
+%   dy        [6x1]   Derivative of the state vector:
+%                     [vx, vy, vz, ax, ay, az] [km/s, km/s^2]
+%
+%--------------------------------------------------------------------------
+% Group number : 27
+%
+% Created and maintained by : 
+%   Azevedo Da Silva Esteban
+%   Gavidia Pantoja Maria Paulina
+%   Donati Filippo 
+%   Domenichelli Eleonora
+%
+%--------------------------------------------------------------------------
 
 r = y(1 : 3);
 v = y (4 : 6);
