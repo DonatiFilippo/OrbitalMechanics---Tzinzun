413bbe7c22690ebee7ffc5a6efdc7a9336211cac
Maria Paulina <75094629+MPaulinaPavidia@users.noreply.github.com>
2024-12-27 07:11:44 +0100

    Update porkchop.m

--- MATLAB functions-20241215/porkchop.m
+++ MATLAB functions-20241215/porkchop.m
@@ -1,5 +1,35 @@
 function [delta_v1, delta_v2] = porkchop(dep_dates_1, arr_dates_1, dep_dates_2, arr_dates_2)
-
+%% FUNCTION DESCRIPTION
+%
+%--------------------------------------------------------------------------
+% DESCRIPTION:
+% This function calculates the delta-v for two consecutive transfer arcs in 
+% order to compute the porkchop plot contours, which represent the delta-v 
+% cost for various  combinations of departure and arrival dates. 
+% These plots help to identify optimal transfer windows.
+%
+%--------------------------------------------------------------------------
+% INPUTS:
+%   dep_dates_1    [nx1]   Array of departure dates for the first leg [mjd2000]
+%   arr_dates_1    [nx1]   Array of arrival dates for the first leg   [mjd2000]
+%   dep_dates_2    [nx1]   Array of departure dates for the second leg [mjd2000]
+%   arr_dates_2    [nx1]   Array of arrival dates for the second leg   [mjd2000]
+%
+%--------------------------------------------------------------------------
+% OUTPUTS:
+%   delta_v1       [nxm]   Matrix of delta-v values for the first transfer leg [km/s]
+%   delta_v2       [pxq]   Matrix of delta-v values for the second transfer leg [km/s]
+%--------------------------------------------------------------------------
+% Group number : 27
+%
+% Created and maintained by : 
+%
+% Azevedo Da Silva Esteban
+% Gavidia Pantoja Maria Paulina
+% Donati Filippo 
+% Domenichelli Eleonora
+% 
+%--------------------------------------------------------------------------
 
 % Inicialización de matrices
 delta_v1 = nan(length(dep_dates_1), length(arr_dates_1));
