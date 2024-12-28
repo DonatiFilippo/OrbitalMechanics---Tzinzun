92cfb2c318e8cde12944217093d4ea41be10eded
Eleonora-Domenichelli <eleonorad0203@gmail.com>
2024-12-28 16:55:59 +0100

    aJ2 commented

--- Eleonora/aJ2.m
+++ Eleonora/aJ2.m
@@ -1,32 +1,42 @@
-function aJ2 = aJ2(r, mu, R, J2)
-% Evaluate the perturbing acceleration due to second zonal harmonic J2,
-% expressed in Geocentric equatorial reference frame
+function a = aJ2(r, mu, R, J2)
+
+% aJ2 - Perturbing acceleration due to second zonal harmonic J2.
 %
 % PROTOTYPE:
-%  a = perturb_acc(y, mu, R, J2)
+%   a = aJ2(r, mu, R, J2)
+%
+% DESCRIPTION:
+%   The function evaluates the perturbing acceleration due to second zonal 
+%   harmonic J2 on a body at distance |r| from teh primary, expressed in
+%   Geocentric Equatorial reference frame.
+%   This frame {x,y,z} is characterised by:
+%       x-axis: on the equatorial plane, along the direction of the gamma
+%           point
+%       z-axis: direction of the north pole
+%       y-axis: on the equatorial plane, completes the reference frame
 %
 % INPUT:
-% y[6x1]     Position of the body in ECI (rx, ry, rz)            [L, L/T]
-% mu[1x1]    Gravitational parameter of the primary              [L^3/T^2]
-% R[1x1]     Primary's radius                                    [L]
-% J2[1x1]    Gravitational harmonic coefficient of the primary   [-]
+%   r[3x1]   Position of the body in Cartesian coordinates       [km]
+%   mu[1x1]  Gravitational parameter of the primary              [km^3/s^2]
+%   R[1x1]   Radius of the primary                               [L]
+%   J2[1x1]  Gravitational harmonic coefficient of the primary   [-]
 %
 % OUTPUT: 
-% a[3X1]     Perturbing acceleration due to J2                    [L/T^2]
+%    a[3X1]  Perturbing acceleration due to J2                   [km/s^2]
 %
 % CONTRIBUTORS:
-%  Eleonora Domenichelli
-%
-% VERSION:
-%  2024/12/22: First version
+%   Azevedo Da Silva Esteban
+%   Gavidia Pantoja Maria Paulina
+%   Donati Filippo 
+%   Domenichelli Eleonora
 %
 %-------------------------------------------------------------------------
 
 % Distance from the primary
 r_norm = norm(r);
 
-% Perturbing acceleration due to J2
-aJ2 = 3/2*( J2*mu*R^2 )/r_norm^4 * [r(1)/r_norm * (5*(r(3)/r_norm)^2-1);...
+% Perturbing acceleration
+a = 3/2*( J2*mu*R^2 )/r_norm^4 * [r(1)/r_norm * (5*(r(3)/r_norm)^2-1);...
                                  r(2)/r_norm * (5*(r(3)/r_norm)^2-1);...
                                  r(3)/r_norm * (5*(r(3)/r_norm)^2-3)];
 
